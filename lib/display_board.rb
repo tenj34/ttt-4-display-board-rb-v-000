d27c45b45aad739f88d8c9e234bc08e6df03055e
Juan Tenezaca <tenj34@gmail.com>
2018-06-09 16:28:00 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,9 +5,9 @@
 #board full of x's  board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
 
 def display_board(board)
-  puts "   |   |   "
+  puts "#{board[0]}   |#{board[1]}   | #{board[2]}  "
   puts "-----------"
-  puts "   |   |   "
+  puts "#{board[3]}   |#{board[4]}   | #{board[5]}  "
   puts "-----------"
-  puts "   |   |   "
+  puts "#{board[6]}  |#{board[7]}   |#{board[7]}   "
 end
