97b0e096509fc18abfc3fc86d554783aaa694e17
Juan Tenezaca <tenj34@gmail.com>
2018-06-09 16:00:50 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,24 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+# Empty board = [" "," "," "," "," "," "," "," "," "]
+# board with an x in the middle board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+
+def board
+  2.times{
+    puts"   |   |   "
+    puts "-----------"
+  }
+  puts"   |   |   "
+end
+
+
+
+
+
+def display_board(board)
+  
+
+
+
+
+end
