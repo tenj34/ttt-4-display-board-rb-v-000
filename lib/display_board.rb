9793da5793cac606e0ece1b130e5c047d0856afd
Juan Tenezaca <tenj34@gmail.com>
2018-06-09 16:21:01 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,16 +2,12 @@
 # out the current state.
 # Empty board = [" "," "," "," "," "," "," "," "," "]
 # board with an x in the middle board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-
-
-
-
-
+#board full of x's  board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
 
 def display_board(board)
-  2.times{
-    puts"   |   |   "
-    puts "-----------"
-  }
-  puts"   |   |   "
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
